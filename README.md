f803a4199743979dec275f3cc1ebfaf0b0e0df74
Rodrigo <rofaccess@gmail.com>
2022-11-14 07:11:33 -0300

    Added NavBar component with logo

--- README.md
+++ README.md
@@ -22,3 +22,30 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Proceso de Desarrollo
+**Verificar que vue cli esté instalado**
+
+    $ vue --version
+
+**Crear el proyecto**
+
+    $ vue create maas-client
+
+**Obs.:** Seleccionar la configuración por defecto de Vue 3.
+
+**Ingresar a la carpeta del proyecto**
+
+    $ cd maas-client
+
+**Iniciar la aplicación**
+
+    $ npm run start
+
+Acceder a localhost:8080 en el navegador
+
+**Instalar Materialize e íconos de Material Design **
+
+    $ npm install materialize-css@next --save
+    $ npm install material-design-icons --save
+
---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo center">
+          <!-- Logo image obtained from: https://codepen.io/maheshambure21/pen/MWWgyyG -->
+          <img alt="MaaS" src="../assets/maas-logo.png" height="56" width="205">
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="companies.html">Companies</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .brand-logo {
+    margin-top: 5px;
+  }
+</style>
