d02f8a5ff62b771f4a06fb6f29a5698087fc011e
Rodrigo <rofaccess@gmail.com>
2022-11-17 20:25:21 -0300

    Added auxiliar files

---
+++ src/App_aux.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav-bar></nav-bar>
+  <router-view :timeBlocks="timeBlocks"></router-view>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import MaterializeHelper from "@/helpers/materialize-helper";
+
+export default {
+  name: 'App',
+  components: { NavBar },
+  data() {
+    return {
+      timeBlocks: []
+    }
+  },
+  mounted() {
+    this.loadTimeBlocks();
+  },
+  methods: {
+    loadTimeBlocks() {
+      this.axios.get('/time_blocks', {responseType: 'json'})
+          .then(response => {
+            this.timeBlocks = response.data;
+            if(!this.hasTimeBlocks) MaterializeHelper.showAlert('No time blocks found');
+          })
+          .catch(error => {
+            MaterializeHelper.showAlert(`${error.message}: Can't load time blocks`, 'danger');
+          });
+    }
+  },
+  computed: {
+    hasTimeBlocks() {
+      return this.timeBlocks.length > 0;
+    }
+  }
+}
+</script>
+
+<style>
+.filter-container {
+  border: 2px solid #ef9a9a;
+}
+
+.row {
+  margin-bottom: 0;
+  margin-top: 20px;
+}
+</style>
---
+++ src/views/ShiftAssignment_aux.vue
@@ -0,0 +1,17 @@
+<template>
+  <shift-assignment-filter></shift-assignment-filter>
+  <weekly-calendar :timeBlocks="timeBlocks"></weekly-calendar>
+</template>
+
+<script>
+import ShiftAssignmentFilter from "@/components/ShiftAssignmentFilter";
+import WeeklyCalendar from "@/components/WeeklyCalendar";
+
+export default {
+  name: 'ShiftAssignment',
+  components: {WeeklyCalendar, ShiftAssignmentFilter},
+  props: {
+    timeBlocks: Array
+  }
+}
+</script>
